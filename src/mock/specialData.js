2ea3ec3f4f1b94270dc1829430ffb17d38eaad7d
m9nawjha <m.nawjha@gmail.com>
2024-03-16 12:19:29 +0200

    last edit

--- src/mock/specialData.js
+++ src/mock/specialData.js
@@ -5,19 +5,19 @@ const SPECIAL_DATA = {
       key: 1,
       title: 'Fresh food',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras.',
-      imgSrc: 'assets/special_1.svg'
+      imgSrc: 'assets/special_3.svg'
     },
     {
       key: 2,
       title: 'skilled Chef',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras.',
-      imgSrc: 'assets/special_2.svg'
+      imgSrc: 'assets/special_1.svg'
     },
     {
       key: 3,
       title: 'Exotic dishes',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras.',
-      imgSrc: 'assets/special_3.svg'
+      imgSrc: 'assets/special_2.svg'
     }
   ]
 };
